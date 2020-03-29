e046c910b7a3dbbb14c8dfba95644228fe0ed406
Adam Morley <adam.morley@gmail.com>
2020-03-29 13:14:41 -0700

    a

--- intHeap_test.go
+++ intHeap_test.go
@@ -1,13 +1,13 @@
 package intHeap
 
 import (
-    "container/heap"
+	"container/heap"
 	"math/rand"
 	"testing"
 )
 
 func TestIntHeap(test *testing.T) {
-    h:=&IntHeap{}
+	h := &IntHeap{}
 	heap.Init(h)
 	m := make(map[int]bool)
 	for i := 0; i < 20; i++ {
@@ -18,12 +18,12 @@ func TestIntHeap(test *testing.T) {
 		heap.Push(h, j)
 	}
 	p := heap.Pop(h).(int)
-    var c int
+	var c int
 	for h.Len() > 0 {
-        c = heap.Pop(h).(int)
-        if c < p {
-            test.Error("popped a value that's less than previous value!")
-        }
-        p = c
+		c = heap.Pop(h).(int)
+		if c < p {
+			test.Error("popped a value that's less than previous value!")
+		}
+		p = c
 	}
 }
